fe527b9b7376e65517c0aa846a4543906b694503
chenmin <chenmin>
2019-08-09 01:26:40 +0800

    ...

---
+++ src/main/java/com/github/jfinal/annotation/RouterPath.java
@@ -0,0 +1,1 @@
+package com.github.jfinal.annotation;import java.lang.annotation.*;/** * 定义Jfinal Controller路由 * @author chenmin */@Target({ElementType.TYPE})@Retention(RetentionPolicy.RUNTIME)@Documented@Inheritedpublic @interface RouterPath {    /**     * 路由值     * @return     */    String value() default "";}
